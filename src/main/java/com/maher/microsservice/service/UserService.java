5cb64d99ee78883cb504d7d7f24afb888d748aea
maher mansour <105998561+mahermansour47455@users.noreply.github.com>
2023-10-23 17:06:03 -0400

    Add files via upload

---
+++ src/main/java/com/maher/microsservice/service/UserService.java
@@ -0,0 +1,32 @@
+package com.maher.microsservice.service;
+
+import com.maher.microsservice.entities.Role;
+import com.maher.microsservice.entities.User;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    User findUserByUsername (String username);
+    Role addRole(Role role);
+    User addRoleToUser(long id, Role r);
+
+    List<User> findAllUsers();
+
+    User findUserById(Long id);
+
+    List<Role> findAllRoles();
+
+    Role findRoleById(Long id);
+
+
+    void deleteUser(long id);
+
+    User removeRoleFromUser(long id, Role r);
+
+    User activateUser(String username , String code);
+
+
+}
+
